cba4f324fedf390328b54d2310ef8c982b4585d2
Siddharth <rambosid1401@gmail.com>
2024-10-08 13:55:02 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,222 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  // {
+  //   id: 5,
+  //   title: "Currently building a JS Animation library",
+  //   description: "The Inside Scoop",
+  //   className: "md:col-span-3 md:row-span-2",
+  //   imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+  //   titleClassName: "justify-center md:justify-start lg:justify-center",
+  //   img: "/b5.svg",
+  //   spareImg: "/grid.svg",
+  // },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Threads Social Media App Clone",
+    des: "The Threads Social Media App Clone is a fully functional replica of a popular social media platform, designed to showcase my skills in building scalable and interactive web applications.",
+    img: "/p1.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "https://full-stack-threads-app-clone.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "User Management App",
+    des: "The User Management App is a comprehensive solution designed to streamline and simplify user administration tasks.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/dock.svg"],
+    link: "/",
+  },
+  {
+    id: 3,
+    title: "New Portfolio Website",
+    des: "I’ve recently revamped my portfolio website using Next.js, showcasing my latest work and skills as a Full-Stack Developer.",
+    img: "/p3.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg"],
+    link: "",
+  },
+  {
+    id: 4,
+    title: "Old Portfolio Website",
+    des: "I’ve recently revamped my portfolio website using Next.js, showcasing my latest work and skills as a Full-Stack Developer.",
+    img: "/p4.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://portfolio-website-seven-cyan-12.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Siddharth consistently proves himself as a reliable and industrious team member, exhibiting unwavering dedication to his responsibilities. His consistent performance and proactive attitude make him an invaluable asset to the team. His contributions significantly boost the team's success",
+    name: "Surya",
+    title: "Manager at Flynava Technologies",
+  },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "yababa",
+  //   title: "yoooo",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    "id": 1,
+    "title": "Frontend Engineer Intern",
+    "desc": "I recently completed an internship at FlyNava Technologies, where I had the opportunity to work on various challenging projects.<br><br>Key aspects of my role included:<br><br>- Building Modules: Developed several modules from scratch, including static and dynamic features.<br><br>- Functionality Enhancements: Implemented update and delete functionalities, and integrated API data fetching and mapping for dynamic tables.<br><br>- User Interface: Designed and developed custom components, with features such as color coding of table contents and query forms for better data interaction.<br><br>- Performance Optimization: Focused on optimizing data flow and improving the performance and responsiveness of applications in a live environment.<br><br>- User Experience: Ensured a seamless and intuitive user experience through meticulous attention to detail and functionality.<br><br>This internship allowed me to work extensively with React, TypeScript, and Redux, enhancing my skills in creating engaging and efficient web interfaces.",
+    "className": "md:col-span-2",
+    "thumbnail": "/exp1.svg"
+  }
+
+
+  // {
+  //   id: 2,
+  //   title: "Mobile App Dev - JSM Tech",
+  //   desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp2.svg",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Freelance App Dev Project",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/Siddharth1401"
+  },
+  // {
+  //   id: 2,
+  //   img: "/gmail.svg",
+  //   url: "https://github.com"
+  // },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/siddharth-saxena-8314431aa/"
+  },
+];
